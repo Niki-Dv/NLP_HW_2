a32b6a921a2fdc9fd120839f9af4351676ce70e9
Niki Davarashvili <niki590@gmail.com>
2021-06-23 16:35:50 +0300

    init dir

---
+++ code/dataset.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+from torch.utils.data.dataloader import DataLoader
+from collections import defaultdict
+from torchtext.vocab import Vocab
+from torch.utils.data.dataset import Dataset, TensorDataset
+from pathlib import Path
+from collections import Counter
+from os.path import join as opj
+
+
+data_dir = r"C:\git-projects\NLP_HW_2\data"
+path_train = opj(data_dir, "train.labeled")
+path_test = opj(data_dir, "test.labeled")
+
+UNKNOWN_TOKEN = "<unk>"
+PAD_TOKEN = "<pad>"  # Optional: this is used to pad a batch of sentences in different lengths.
+# ROOT_TOKEN = PAD_TOKEN # this can be used if you are not padding your batches
+ROOT_TOKEN = "<root>" # use this if you are padding your batches and want a special token for ROOT
+SPECIAL_TOKENS = [ROOT_TOKEN, PAD_TOKEN, UNKNOWN_TOKEN]
+
+########################################################################################
+def split(string, delimiters):
+    """
+        Split strings according to delimiters
+        :param string: full sentence
+        :param delimiters string: characters for spliting
+            function splits sentence to words
+    """
+    delimiters = tuple(delimiters)
+    stack = [string, ]
+
+    for delimiter in delimiters:
+        for i, substring in enumerate(stack):
+            substack = substring.split(delimiter)
+            stack.pop(i)
+            for j, _substring in enumerate(substack):
+                stack.insert(i + j, _substring)
+
+    return stack
+
+########################################################################################
+def get_vocabs(list_of_paths):
+    """
+        Extract vocabs from given datasets. Return a word2ids and tag2idx.
+        :param file_paths: a list with a full path for all corpuses
+            Return:
+              - word2idx
+              - tag2idx
+    """
+    word_dict = defaultdict(int)
+    pos_dict = defaultdict(int)
+    for file_path in list_of_paths:
+        with open(file_path) as f:
+            for line in f:
+                if line == "\n":
+                    continue
+
+                splited_words = split(line, (' ', '\n', '\t', '_'))
+
+                while '' in splited_words:
+                    splited_words.remove('')
+
+                del splited_words[-1]
+                word = splited_words[1].lower()
+                pos_tag = splited_words[2]
+
+                word_dict[word] += 1
+                pos_dict[pos_tag] += 1
+
+    return word_dict, pos_dict
+
+########################################################################################
+class PosDataReader:
+    def __init__(self, file, word_dict, pos_dict):
+        self.file = file
+        self.word_dict = word_dict
+        self.pos_dict = pos_dict
+        self.sentences = []
+        self.__readData__()
+
+    def __readData__(self):
+        """main reader function which also populates the class data structures"""
+        cur_sentence = []
+        with open(self.file, 'r') as f:
+            for line in f:
+                if line == "\n":
+                    self.sentences.append(cur_sentence)
+                    cur_sentence = [(ROOT_TOKEN, ROOT_TOKEN, -1)]
+                    #cur_sentence = []
+                    continue
+
+                splited_words = split(line, (' ', '\n', '\t', '_'))
+
+                while '' in splited_words:
+                    splited_words.remove('')
+                del splited_words[-1]
+
+                word = splited_words[1].lower()
+                pos_tag = splited_words[2]
+                head_idx = int(splited_words[3])
+                cur_sentence.append((word, pos_tag, head_idx))
+
+    def get_num_sentences(self):
+        """returns num of sentences in data"""
+        return len(self.sentences)
+
+########################################################################################
+
+
+class PosDataset(Dataset):
+    def __init__(self, word_dict, pos_dict, dir_path: str, subset: str,
+                 padding=False, word_embeddings=None):
+        """
+        :param word_dict:
+        :param pos_dict:
+        :param dir_path:
+        :param subset:
+        :param padding:
+        :param word_embeddings:
+        """
+        super().__init__()
+
+        assert subset in ['train', 'test']
+        self.subset = subset
+
+        self.file = opj(dir_path, subset + ".labeled")
+
+        self.datareader = PosDataReader(self.file, word_dict, pos_dict)
+
+        self.vocab_size = len(self.datareader.word_dict)
+
+        if word_embeddings:
+            self.word_idx_mappings, self.idx_word_mappings, self.word_vectors = word_embeddings
+        else:
+            self.word_idx_mappings, self.idx_word_mappings, self.word_vectors =\
+                self.init_word_embeddings(self.datareader.word_dict)
+
+        self.pos_idx_mappings, self.idx_pos_mappings = self.init_pos_vocab(self.datareader.pos_dict)
+
+        self.pad_idx = self.word_idx_mappings.get(PAD_TOKEN)
+        self.unknown_idx = self.word_idx_mappings.get(UNKNOWN_TOKEN)
+        self.word_vector_dim = self.word_vectors.size(-1)
+        self.sentence_lens = [len(sentence) for sentence in self.datareader.sentences]
+        self.max_seq_len = max(self.sentence_lens)
+        self.sentences_dataset = self.convert_sentences_to_dataset(padding)
+
+    def __len__(self):
+        return len(self.sentences_dataset)
+
+    def __getitem__(self, index):
+        word_embed_idx, pos_embed_idx, head_idx, sentence_len = self.sentences_dataset[index]
+        return word_embed_idx, pos_embed_idx, head_idx, sentence_len
+
+    @staticmethod
+    def init_word_embeddings(word_dict):
+        glove = Vocab(Counter(word_dict), vectors="glove.6B.300d", specials=SPECIAL_TOKENS)
+        return glove.stoi, glove.itos, glove.vectors
+
+    def get_word_embeddings(self):
+        return self.word_idx_mappings, self.idx_word_mappings, self.word_vectors
+
+    def init_pos_vocab(self, pos_dict):
+        idx_pos_mappings = sorted([self.word_idx_mappings.get(token) for token in SPECIAL_TOKENS])
+        pos_idx_mappings = {self.idx_word_mappings[idx]: idx for idx in idx_pos_mappings}
+
+        for i, pos in enumerate(sorted(pos_dict.keys())):
+            # pos_idx_mappings[str(pos)] = int(i)
+            pos_idx_mappings[str(pos)] = int(i + len(SPECIAL_TOKENS))
+            idx_pos_mappings.append(str(pos))
+        #print("idx_pos_mappings -", idx_pos_mappings)
+        #print("pos_idx_mappings -", pos_idx_mappings)
+        return pos_idx_mappings, idx_pos_mappings
+
+    def get_pos_vocab(self):
+        return self.pos_idx_mappings, self.idx_pos_mappings
+
+    def convert_sentences_to_dataset(self, padding):
+        sentence_word_idx_list = list()
+        sentence_pos_idx_list = list()
+        sentence_head_idx_list = list()
+        sentence_len_list = list()
+        for sentence_idx, sentence in enumerate(self.datareader.sentences):
+            words_idx_list = []
+            pos_idx_list = []
+            head_idx_list = []
+            for word, pos, head_idx in sentence:
+                words_idx_list.append(self.word_idx_mappings.get(word))
+                pos_idx_list.append(self.pos_idx_mappings.get(pos))
+                head_idx_list.append(head_idx)
+
+            sentence_len = len(words_idx_list)
+            # if padding:
+            #     while len(words_idx_list) < self.max_seq_len:
+            #         words_idx_list.append(self.word_idx_mappings.get(PAD_TOKEN))
+            #         pos_idx_list.append(self.pos_idx_mappings.get(PAD_TOKEN))
+
+            sentence_word_idx_list.append(torch.tensor(words_idx_list, dtype=torch.long, requires_grad=False))
+            sentence_pos_idx_list.append(torch.tensor(pos_idx_list, dtype=torch.long, requires_grad=False))
+            sentence_head_idx_list.append(torch.tensor(head_idx_list, dtype=torch.long, requires_grad=False))
+            sentence_len_list.append(sentence_len)
+
+        # if padding:
+        #     all_sentence_word_idx = torch.tensor(sentence_word_idx_list, dtype=torch.long)
+        #     all_sentence_pos_idx = torch.tensor(sentence_pos_idx_list, dtype=torch.long)
+        #     all_sentence_len = torch.tensor(sentence_len_list, dtype=torch.long, requires_grad=False)
+        #     return TensorDataset(all_sentence_word_idx, all_sentence_pos_idx, all_sentence_len)
+
+        return {i: sample_tuple for i, sample_tuple in enumerate(zip(sentence_word_idx_list,
+                                                                     sentence_pos_idx_list,
+                                                                     sentence_head_idx_list,
+                                                                     sentence_len_list))}
+
+########################################################################################
+
+
+
---
+++ code/train_main.py
@@ -0,0 +1,182 @@
+import os
+import sys
+from typing import Callable, Any
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+from torch.nn import LogSoftmax, NLLLoss
+from torch.utils.data.dataloader import DataLoader
+from collections import defaultdict
+from torchtext.vocab import Vocab
+from torch.utils.data.dataset import Dataset, TensorDataset
+from pathlib import Path
+from collections import Counter
+from os.path import join as opj
+import pathlib
+import time
+from chu_liu_edmonds import decode_mst
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+curr_dir = pathlib.Path(__file__).parent.absolute()
+
+if curr_dir not in sys.path:
+    sys.path.append(curr_dir)
+
+data_dir = opj(curr_dir, '..\\data')
+net_results_dir = opj(curr_dir, '..\\net_results')
+path_train = opj(data_dir, "train.labeled")
+path_test = opj(data_dir, "test.labeled")
+
+torch.manual_seed(1)
+import dataset, models
+
+
+DEBUG = 500
+
+def plot_net_results(acc, loss, epoch, dir_save_path, prefix_str=""):
+    fig, ax1 = plt.subplots(figsize=(12, 4))
+    ax1.plot(list(range(1, 1 + len(loss))), loss)
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss')
+    ax1.tick_params(axis='y')
+
+    ax2 = ax1.twinx()
+    ax2.plot(list(range(len(acc))), acc)
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('UAS')
+    ax2.tick_params(axis='y')
+
+    fig.tight_layout()
+    fig.suptitle(f'Results summary for epoch: {epoch} ')
+    fig.savefig(opj(dir_save_path, prefix_str + f"prog_plot_epoch {epoch}"))
+    plt.close()
+
+def nll_loss_func(scores, target):
+    """
+
+    :param scores: [batch_size, seq_length, seq_length]
+    :param target:  [batch_size, seq_length]
+    :param nllloss
+    :return:
+    """
+    output= 0
+    nllloss = NLLLoss(ignore_index=-1)
+    m = LogSoftmax(dim=1)
+    output = nllloss(m(scores), target)
+    return output
+
+def predict_dep(scores):
+    predictions = []
+    for sentence_scores in scores:
+        score_matrix = sentence_scores.cpu().detach().numpy()
+        score_matrix[:, 0] = float("-inf")
+        mst, _ = decode_mst(score_matrix, len(score_matrix), has_labels=False)
+        predictions.append(mst)
+    return np.array(predictions)
+
+def predict(net, device, loader, loss_func):
+    net.eval()
+    acc, num_of_edges, loss = 0, 0, 0
+    for i, sentence in enumerate(tqdm(loader)):
+        if DEBUG is not None and i == DEBUG:
+            break
+        headers = sentence[2].to(device)
+        scores = net(sentence)
+        loss += loss_func(scores, headers).item()
+        predictions = predict_dep(scores)[:, 1:]
+        headers = headers.to("cpu").numpy()[:, 1:]
+        acc += np.sum(headers == predictions)
+        num_of_edges += predictions.size
+    net.train()
+    return acc / num_of_edges, loss
+
+def train_net(net, train_dataloader, test_dataloader, loss_func: Callable, EPOCHS = 15, BATCH_SIZE = 1, lr=0.001,
+              plot_progress=True, results_dir_path='.'):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+    net.train()
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    print("Training Started")
+    test_loss_lst, test_acc_lst, train_loss_lst, train_acc_lst = [], [], [], []
+    best_acc = 0
+
+    for epoch in range(EPOCHS):
+        t0 = time.time()
+        NUM_WORDS_BATCH = 0
+        for i, sentence in enumerate(tqdm(train_dataloader)):
+            if DEBUG is not None and i == DEBUG:
+                break
+            headers = sentence[2].to(device)
+            sentence_len = sentence[3]
+            scores = net(sentence)
+            loss = loss_func(scores, headers) * sentence_len
+            NUM_WORDS_BATCH += sentence_len
+            loss.backward()
+
+            if i % BATCH_SIZE == 0:
+                loss /= NUM_WORDS_BATCH
+                train_loss_lst.append(train_loss)
+                train_acc_lst.append(train_acc)
+                optimizer.step()
+                net.zero_grad()
+                NUM_WORDS_BATCH = 0
+
+        test_acc, test_loss = predict(net, device, test_dataloader, loss_func)
+        test_loss_lst.append(test_loss)
+        test_acc_lst.append(test_acc)
+
+        if best_acc < test_acc and epoch > 5 and test_acc > 0.7:
+            save_path = opj(results_dir_path, '_epoch_' + str(epoch) + '_acc_' + str(np.round(test_acc, 4)) + '.pt')
+            net.save(save_path)
+            best_acc = test_acc
+
+        if plot_progress:
+            train_acc, train_loss = predict(net, device, train_dataloader, loss_func)
+
+        print(f"Epoch [{epoch + 1}/{EPOCHS}]. \t Test Loss: {test_loss:.2f}"
+              f" \t Test Accuracy: {test_acc:.2f}."
+              f"Train average Loss: {np.average(train_loss_lst):.2f}"
+              f" Time for epoch: {time.time()-t0}")
+
+    plot_net_results(test_acc_lst, test_loss_lst, epoch, results_dir_path, 'test_res_plots')
+
+    if plot_progress:
+        plot_net_results(train_acc_lst, train_loss_lst, epoch, results_dir_path, 'train_res_plots.png')
+
+##################################################################################################################
+if __name__ == '__main__':
+    WORD_EMBEDDING_DIM = 300
+    TAG_EMBEDDING_DIM = 46
+    HIDDEN_DIM = 100
+    BATCH_SIZE = 40
+    EPOCHS = 15
+    LR = 0.01
+
+    paths_list = [path_train, path_test]
+    word_dict, pos_dict = dataset.get_vocabs(paths_list)
+    train_dataset = dataset.PosDataset(word_dict, pos_dict, data_dir, 'train', padding=False)
+    train_dataloader = DataLoader(train_dataset, shuffle=True)
+
+    test_dataset = dataset.PosDataset(word_dict, pos_dict, data_dir, 'test', padding=False)
+    test_dataloader = DataLoader(test_dataset, shuffle=False)
+
+    word_vocab_size = len(train_dataset.word_idx_mappings)
+    tag_vocab_size = len(train_dataset.pos_idx_mappings)
+
+    base_model = models.BasicDependencyParserModel(word_vocab_size, tag_vocab_size, WORD_EMBEDDING_DIM, TAG_EMBEDDING_DIM,
+                                              hidden_dim=HIDDEN_DIM, mlp_dim_out=32)
+
+    res_dir = opj(net_results_dir,time.strftime("%Y%m%d-%H%M%S") )
+    if not res_dir:
+        os.makedirs(res_dir)
+    train_net(base_model, train_dataloader, test_dataloader, nll_loss_func, EPOCHS=EPOCHS, BATCH_SIZE=BATCH_SIZE, lr=LR,
+              plot_progress=True, results_dir_path= res_dir)
+
+
+
