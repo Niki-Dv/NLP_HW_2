7ed8c350f18763f63fdc29ab1d14ae6d22adf8f1
Niki Davarashvili <niki590@gmail.com>
2021-06-25 14:13:37 +0300

    update

--- code/train_main.py
+++ code/train_main.py
@@ -37,7 +37,7 @@ import dataset, models
 
 
 DEBUG = None
-
+##################################################################################################################
 def plot_net_results(acc_list, loss_list, epoch, dir_save_path, prefix_str=""):
     f = plt.figure()
     axes = f.add_subplot(111)
@@ -53,9 +53,10 @@ def plot_net_results(acc_list, loss_list, epoch, dir_save_path, prefix_str=""):
 
     f.tight_layout()
     f.suptitle(f'Results summary for epoch: {epoch} ')
-    f.savefig(opj(dir_save_path, prefix_str + f"prog_plot_epoch {epoch}"))
+    f.savefig(opj(dir_save_path, prefix_str + f"final acc_{round(acc_list[-1],3)}_prog_plot_epoch {epoch}"))
     plt.close('all')
 
+##################################################################################################################
 def nll_loss_func(scores, target):
     """
 
@@ -64,12 +65,12 @@ def nll_loss_func(scores, target):
     :param nllloss
     :return:
     """
-    output= 0
     nllloss = NLLLoss(ignore_index=-1)
     m = LogSoftmax(dim=1)
     output = nllloss(m(scores), target)
     return output
 
+##################################################################################################################
 def predict_edges(scores):
     edge_predictions = []
     for sentence_scores in scores:
@@ -93,12 +94,13 @@ def predict(net, device, loader, loss_func):
         headers = headers.to("cpu").numpy()[:, 1:]
         acc += np.sum(headers == predictions)
         num_of_edges += predictions.size
+
     net.train()
-
-    return acc / num_of_edges, loss/len(loader)
-
+    return acc / num_of_edges, loss / num_of_edges
+##################################################################################################################
 def train_net(net, train_dataloader, test_dataloader, loss_func: Callable, EPOCHS = 15, BATCH_SIZE = 1, lr=0.001,
               plot_progress=True, results_dir_path='.'):
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Device used is: {device}")
     net.to(device)
@@ -110,24 +112,27 @@ def train_net(net, train_dataloader, test_dataloader, loss_func: Callable, EPOCH
     best_acc = 0
     for epoch in range(EPOCHS):
         t0 = time.time()
-        NUM_WORDS_BATCH = 0
+        total_epoch_loss = 0
+        EPOCH_LOSSES = []
+        num_words_in_batch = 0
         for i, sentence in enumerate(tqdm(train_dataloader)):
             if DEBUG is not None and i == DEBUG:
                 break
             headers = sentence[2].to(device)
-            sentence_len = sentence[3].to(device)
+            num_words_in_batch += sentence[3].item()
             scores = net(sentence)
-            loss = loss_func(scores, headers) * sentence_len
-            NUM_WORDS_BATCH += sentence_len
+            loss = loss_func(scores, headers)
+            total_epoch_loss += loss.item()
             loss.backward()
 
-            if i % BATCH_SIZE == 0:
-                loss /= NUM_WORDS_BATCH
-                train_loss_lst.append(loss.item())
+            if i % BATCH_SIZE == 0 and i > 0:
+                EPOCH_LOSSES.append(total_epoch_loss/num_words_in_batch)
+                total_epoch_loss = 0
+                num_words_in_batch = 0
                 optimizer.step()
                 net.zero_grad()
-                NUM_WORDS_BATCH = 0
 
+        train_loss_lst.append(np.average(EPOCH_LOSSES))
         test_acc, test_loss = predict(net, device, test_dataloader, loss_func)
         test_loss_lst.append(test_loss)
         test_acc_lst.append(test_acc)
@@ -137,10 +142,10 @@ def train_net(net, train_dataloader, test_dataloader, loss_func: Callable, EPOCH
             net.save(save_path)
             best_acc = test_acc
 
-        print(f"Epoch [{epoch + 1}/{EPOCHS}]. \t Test word avg loss: {test_loss:.2f}"
+        print(f"\nEpoch [{epoch + 1}/{EPOCHS}]. \t Test word avg loss: {test_loss:.2f}"
               f" \t Test Accuracy: {test_acc:.2f}."
-              f"Train word avg loss: {np.average(train_loss_lst):.2f}"
-              f" Time for epoch: {time.time()-t0}")
+              f"\t Train sentence avg loss: {np.average(train_loss_lst):.2f}"
+              f"\t Time for epoch: {time.time()-t0}")
 
     plot_net_results([], train_loss_lst, epoch, results_dir_path, 'train_res_plots')
     plot_net_results(test_acc_lst, test_loss_lst, epoch, results_dir_path, 'test_res_plots')
@@ -148,8 +153,41 @@ def train_net(net, train_dataloader, test_dataloader, loss_func: Callable, EPOCH
     if plot_progress:
         plot_net_results([], train_loss_lst, epoch, results_dir_path, 'train_res_plots')
 
+    return np.max(test_acc_lst)
 ##################################################################################################################
-if __name__ == '__main__':
+def run_base_model():
+    WORD_EMBEDDING_DIM = 100
+    TAG_EMBEDDING_DIM = 25
+    LSTM_HIDDEN_DIM = 125
+    MLP_HIDDEN_DIM = 100
+    BATCH_SIZE = 10
+    EPOCHS = 15
+    LR = 0.01
+
+    paths_list = [path_train]
+    word_dict, pos_dict = dataset.get_vocabs(paths_list)
+    train_dataset = dataset.PosDataset(word_dict, pos_dict, data_dir, 'train', padding=False)
+    train_dataloader = DataLoader(train_dataset, shuffle=True)
+
+    test_dataset = dataset.PosDataset(word_dict, pos_dict, data_dir, 'test', padding=False)
+    test_dataloader = DataLoader(test_dataset, shuffle=False)
+
+    word_vocab_size = len(train_dataset.word_idx_mappings)
+    tag_vocab_size = len(train_dataset.pos_idx_mappings)
+
+    base_model = models.BasicDependencyParserModel(word_vocab_size, tag_vocab_size, WORD_EMBEDDING_DIM,
+                                                   TAG_EMBEDDING_DIM,
+                                                   hidden_dim=LSTM_HIDDEN_DIM, mlp_dim_out=MLP_HIDDEN_DIM)
+
+    res_dir = opj(net_results_dir,  "base_model_results" + time.strftime("%Y%m%d-%H%M%S"))
+    if not os.path.isdir(res_dir):
+        os.makedirs(res_dir)
+
+    train_net(base_model, train_dataloader, test_dataloader, nll_loss_func, EPOCHS=EPOCHS, BATCH_SIZE=BATCH_SIZE, lr=LR,
+              plot_progress=True, results_dir_path=res_dir)
+
+##################################################################################################################
+def run_adv_model():
     WORD_EMBEDDING_DIM = 100
     TAG_EMBEDDING_DIM = 25
     LSTM_HIDDEN_DIM = 125
@@ -169,15 +207,32 @@ if __name__ == '__main__':
     word_vocab_size = len(train_dataset.word_idx_mappings)
     tag_vocab_size = len(train_dataset.pos_idx_mappings)
 
-    base_model = models.BasicDependencyParserModel(word_vocab_size, tag_vocab_size, WORD_EMBEDDING_DIM, TAG_EMBEDDING_DIM,
-                                              hidden_dim=LSTM_HIDDEN_DIM, mlp_dim_out=MLP_HIDDEN_DIM)
+    base_model = models.BasicDependencyParserModel(word_vocab_size, tag_vocab_size, WORD_EMBEDDING_DIM,
+                                                   TAG_EMBEDDING_DIM,
+                                                   hidden_dim=LSTM_HIDDEN_DIM, mlp_dim_out=MLP_HIDDEN_DIM)
 
-    res_dir = opj(net_results_dir,time.strftime("%Y%m%d-%H%M%S") )
+    res_dir = opj(net_results_dir, "adv_model_results" + time.strftime("%Y%m%d-%H%M%S"))
+
     if not os.path.isdir(res_dir):
         os.makedirs(res_dir)
 
     train_net(base_model, train_dataloader, test_dataloader, nll_loss_func, EPOCHS=EPOCHS, BATCH_SIZE=BATCH_SIZE, lr=LR,
-              plot_progress=True, results_dir_path= res_dir)
+              plot_progress=True, results_dir_path=res_dir)
+
+def run_different_combos():
+    WORD_EMBEDDING_DIM_OPTIONS = [50, 100, 200, 300]
+    TAG_EMBEDDING_DIM = [25, 50]
+    LSTM_HIDDEN_DIM = [60, 125, 250]
+    MLP_HIDDEN_DIM = [50, 100, 200]
+    BATCH_SIZE = [20, 40]
+    EPOCHS = 15
+    LR = 0.01
+
+##################################################################################################################
+if __name__ == '__main__':
+    run_base_model()
+    #run_adv_model()
+
 
 
 
